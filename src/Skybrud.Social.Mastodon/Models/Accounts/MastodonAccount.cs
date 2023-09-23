b7d79e702aa7d9ca7442290acebe07d85a4f1022
Anders Bjerner <abjerner@skybrud.dk>
2023-09-23 17:28:17 +0200

    Implemented more properties on the "MastodonAccount" model

--- src/Skybrud.Social.Mastodon/Models/Accounts/MastodonAccount.cs
+++ src/Skybrud.Social.Mastodon/Models/Accounts/MastodonAccount.cs
@@ -47,6 +47,11 @@ public class MastodonAccount : MastodonObject {
     public string Url { get; }
 
     /// <summary>
+    /// Gets the URI of the account. <strong>This property is not documented by the Mastodon API documentation.</strong>
+    /// </summary>
+    public string Uri { get; }
+
+    /// <summary>
     /// Gets the profile’s display name.
     /// </summary>
     /// <see>
@@ -69,6 +74,71 @@ public class MastodonAccount : MastodonObject {
     ///     <cref>https://docs.joinmastodon.org/entities/Account/#note</cref>
     /// </see>
     public string Note { get; }
+
+    /// <summary>
+    /// An image icon that is shown next to statuses and in the profile.
+    /// </summary>
+    /// <see>
+    ///     <cref>https://docs.joinmastodon.org/entities/Account/#avatar</cref>
+    /// </see>
+    public string Avatar { get; }
+
+    /// <summary>
+    /// A static version of the avatar. Equal to <see cref="Avatar"/> if its value is a static image; different if <see cref="Avatar"/> is an animated GIF.
+    /// </summary>
+    /// <see>
+    ///     <cref>https://docs.joinmastodon.org/entities/Account/#avatar_static</cref>
+    /// </see>
+    public string AvatarStatic { get; }
+
+    /// <summary>
+    /// An image banner that is shown above the profile and in profile cards.
+    /// </summary>
+    /// <see>
+    ///     <cref>https://docs.joinmastodon.org/entities/Account/#header</cref>
+    /// </see>
+    public string Header { get; }
+
+    /// <summary>
+    /// A static version of the header. Equal to header if its value is a static image; different if header is an animated GIF.
+    /// </summary>
+    /// <see>
+    ///     <cref>https://docs.joinmastodon.org/entities/Account/#header_static</cref>
+    /// </see>
+    public string HeaderStatic { get; }
+
+
+    /// <summary>
+    /// Gets whether the account manually approves follow requests.
+    /// </summary>
+    /// <see>
+    ///     <cref>https://docs.joinmastodon.org/entities/Account/#locked</cref>
+    /// </see>
+    public bool IsLocked { get; }
+
+    /// <summary>
+    /// Gets whether the account may perform automated actions, may not be monitored, or identifies as a robot.
+    /// </summary>
+    /// <see>
+    ///     <cref>https://docs.joinmastodon.org/entities/Account/#bot</cref>
+    /// </see>
+    public bool IsBot { get; }
+
+    /// <summary>
+    /// Gets whether the account has opted into discovery features such as the profile directory.
+    /// </summary>
+    /// <see>
+    ///     <cref>https://docs.joinmastodon.org/entities/Account/#discoverable</cref>
+    /// </see>
+    public bool? IsDiscoverable { get; }
+
+    /// <summary>
+    /// Gets whether the account represents a group actor.
+    /// </summary>
+    /// <see>
+    ///     <cref>https://docs.joinmastodon.org/entities/Account/#group</cref>
+    /// </see>
+    public bool IsGroup { get; }
 
     /// <summary>
     /// Gets the reported followers of this profile.
@@ -94,6 +164,14 @@ public class MastodonAccount : MastodonObject {
     /// </see>
     public int StatusesCount { get; }
 
+    /// <summary>
+    /// Gets when the most recent status was posted, or <see langword="null"/> if the account hasn't posted yet.
+    /// </summary>
+    /// <see>
+    ///     <cref>https://docs.joinmastodon.org/entities/Account/#last_status_at</cref>
+    /// </see>
+    public EssentialsDate? LastStatusAt { get; }
+
     #endregion
 
     #region Constructors
@@ -107,12 +185,22 @@ public class MastodonAccount : MastodonObject {
         Username = json.GetString("username")!;
         Acct = json.GetString("acct")!;
         Url = json.GetString("url")!;
+        Uri = json.GetString("uri")!;
         DisplayName = json.GetString("display_name")!;
         CreatedTime = json.GetString("created_at", EssentialsTime.Parse)!;
         Note = json.GetString("note")!;
+        Avatar = json.GetString("avatar")!;
+        AvatarStatic = json.GetString("avatar_static")!;
+        Header = json.GetString("header")!;
+        HeaderStatic = json.GetString("hedder_static")!;
+        IsLocked = json.GetBoolean("locked");
+        IsBot = json.GetBoolean("bot");
+        IsDiscoverable = json.GetBooleanOrNull("discoverable");
+        IsGroup = json.GetBoolean("group");
         FollowersCount = json.GetInt32("followers_count");
         FollowingCount = json.GetInt32("following_count");
         StatusesCount = json.GetInt32("statuses_count");
+        LastStatusAt = json.GetString("last_status_at", EssentialsDate.Parse);
     }
 
     #endregion
